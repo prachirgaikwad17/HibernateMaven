8fb0d526f412c40f0e4b569c150e7a8e31c75e82
Prachi Gaikwad <prachi321g@gmail.com>
2019-10-01 11:35:47 +0530

    Initial commit.Added SimpleClassMappingMavenProject.

---
+++ SimpleClassMappingMaven/src/main/java/com/zensar/hibernate/HibernateMain.java
@@ -0,0 +1,34 @@
+package com.zensar.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.zensar.hibernate.entities.Product;
+
+/**
+ * Hello world!
+ *
+ */
+public class HibernateMain 
+{
+    public static void main( String[] args )
+    {
+    	Configuration c=new Configuration().configure();
+    	SessionFactory f=c.buildSessionFactory();
+    	Session s=f.openSession();
+    	Transaction t=s.beginTransaction();
+    	
+    	Product p=new Product();
+    	p.setProductId(1002);
+    	p.setName("Laptop");
+    	p.setBrand("HP");
+    	p.setPrice(50000);    	
+    	s.save(p);
+    	
+    	
+    	t.commit();
+    	s.close();
+    }
+}
